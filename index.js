51d6e41a63bca9051e8345de7cb8b0a3f6f1ae78
Pranathi-star <pranathik2001@gmail.com>
2023-09-03 17:02:56 +0530

    Solution for Exercise 3.4

--- index.js
+++ index.js
@@ -59,6 +59,12 @@ app.get('/api/persons/:id', (req, res) => {
   }
 })
 
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const newPersons = persons.filter(person => person.id !== id)
+  res.status(204).end()
+})
+
 const PORT = 3001
 app.listen(PORT, (err, res) => {console.log(`Server running on port ${PORT}`)
 })
