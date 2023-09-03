91a393e10ae61afe42b2f42bd64792c7400000b3
Pranathi-star <pranathik2001@gmail.com>
2023-09-03 16:54:31 +0530

    Solution for Exercise 3.3

--- index.js
+++ index.js
@@ -40,15 +40,24 @@ let dayMap = {0: "Sun", 1: "Mon", 2: "Tue", 3: "Wed", 4: "Thu", 5: "Fri"}
 let monthMap = {"01": "Jan", "02": "Feb", "03": "Mar", "04": "Apr", "05": "May", "06": "Jun", "07": "Jul", "08": "Aug", "09": "Sep", "10": "Oct", "11": "Nov", "12": "Dec"}
 
 app.get("/info", (req, res) => {
-
-    res.send(
-      `<div><p>Phonebook has entries for ${persons.length} people</p><br /><p>${dayMap[day]} ${monthMap[month]} ${date} ${year} ${time} GMT${sign}${hours}${minutes} (${timeZone})</p></div>`
-    )
+  res.send(
+    `<div><p>Phonebook has entries for ${persons.length} people</p><br /><p>${dayMap[day]} ${monthMap[month]} ${date} ${year} ${time} GMT${sign}${hours}${minutes} (${timeZone})</p></div>`
+  )
 })
 
 app.get('/api/persons', (req, res) => {
-    res.json(persons)
-})  
+  res.json(persons)
+}) 
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const person = persons.find(person => person.id === id)
+  if (person) {
+    res.json(person)
+  } else {
+    res.status(404).end()
+  }
+})
 
 const PORT = 3001
 app.listen(PORT, (err, res) => {console.log(`Server running on port ${PORT}`)
