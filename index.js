652a96e0281de2f7f9a5aa463e35bfccf2df6073
Pranathi-star <pranathik2001@gmail.com>
2023-09-03 16:44:34 +0530

    Solution for Exercise 3.2

--- index.js
+++ index.js
@@ -24,8 +24,26 @@ let persons = [
     }
 ]
 
-app.get("/", (req, res) => {
-    res.send('<h1>Hello World!</h1>')
+let dateTime = new Date()
+let year = dateTime.toJSON().slice(0, 4)
+let month = dateTime.toJSON().slice(5, 7)
+let date = dateTime.toJSON().slice(8, 10)
+let time = dateTime.toJSON().slice(11, dateTime.length - 1)
+let day = dateTime.getDay();
+let timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone
+let offset = dateTime.getTimezoneOffset()
+let sign = offset > 0 ? "-" : "+"
+let hours = Math.floor(Math.abs(offset / 60))
+let minutes = Math.abs(offset % 60)
+
+let dayMap = {0: "Sun", 1: "Mon", 2: "Tue", 3: "Wed", 4: "Thu", 5: "Fri"}
+let monthMap = {"01": "Jan", "02": "Feb", "03": "Mar", "04": "Apr", "05": "May", "06": "Jun", "07": "Jul", "08": "Aug", "09": "Sep", "10": "Oct", "11": "Nov", "12": "Dec"}
+
+app.get("/info", (req, res) => {
+
+    res.send(
+      `<div><p>Phonebook has entries for ${persons.length} people</p><br /><p>${dayMap[day]} ${monthMap[month]} ${date} ${year} ${time} GMT${sign}${hours}${minutes} (${timeZone})</p></div>`
+    )
 })
 
 app.get('/api/persons', (req, res) => {
